33cd0dbf03b67eb5d2ac9d5bc74e4804d761571a
Alli Edwards <edwardsallistair15@gmail.com>
2023-09-06 14:34:38 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+from sklearn import svm, datasets
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+iris = datasets.load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+df['flower'] = iris.target
+df['flower'] = df['flower'].apply(lambda x: iris.target_names[x])
+
+x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3)
+
+from sklearn.model_selection import GridSearchCV
+
+clf = GridSearchCV(svm.SVC(gamma='auto'), {
+    'C': [1,10,20],
+    'kernel': ['rbf','linear']
+}, cv=5, return_train_score=False)
+
+clf.fit(iris.data, iris.target)
+
+df = pd.DataFrame(clf.cv_results_)
+df.to_csv('results.csv', index=False)
+
+from sklearn.model_selection import RandomizedSearchCV
+rs = RandomizedSearchCV(svm.SVC(gamma='auto'), {
+    'C': [1,10,20],
+    'kernel': ['rbf','linear']
+},
+    cv=5, return_train_score=False, n_iter=2)
+rs.fit(iris.data, iris.target)
+df2 = pd.DataFrame(rs.cv_results_)
+df2.to_csv('results_random.csv', index=False)
+
+
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+model_params = {
+    'svm': {
+        'model': svm.SVC(gamma='auto'),
+        'params' : {
+            'C': [1,10,20],
+            'kernel': ['rbf','linear']
+        }
+    },
+    'random_forest': {
+        'model': RandomForestClassifier(),
+        'params' : {
+            'n_estimators': [1,5,10]
+        }
+    },
+    'logistic_regression' : {
+        'model': LogisticRegression(solver='liblinear',multi_class='auto'),
+        'params': {
+            'C': [1,5,10]
+        }
+    }
+}
+
+scores = []
+
+for model_name, mp in model_params.items():
+    clf = GridSearchCV(mp['model'], mp['params'], cv=5, return_train_score=False)
+    clf.fit(iris.data, iris.target)
+    scores.append({
+        'model': model_name,
+        'best_score': clf.best_score_,
+        'best_params': clf.best_params_
+    })
+
+df3 = pd.DataFrame(scores, columns=['model', 'best_score', 'best_params'])
+print(df3)
