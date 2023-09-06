a61753527e1bf015c59b6b00b6d5203e09e9ca97
Alli Edwards <edwardsallistair15@gmail.com>
2023-09-06 14:37:47 +0100

    Cleaned up imports

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from sklearn import svm, datasets
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
 
 iris = datasets.load_iris()
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
@@ -9,8 +13,6 @@ df['flower'] = iris.target
 df['flower'] = df['flower'].apply(lambda x: iris.target_names[x])
 
 x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3)
-
-from sklearn.model_selection import GridSearchCV
 
 clf = GridSearchCV(svm.SVC(gamma='auto'), {
     'C': [1,10,20],
@@ -22,20 +24,14 @@ clf.fit(iris.data, iris.target)
 df = pd.DataFrame(clf.cv_results_)
 df.to_csv('results.csv', index=False)
 
-from sklearn.model_selection import RandomizedSearchCV
 rs = RandomizedSearchCV(svm.SVC(gamma='auto'), {
     'C': [1,10,20],
-    'kernel': ['rbf','linear']
-},
+    'kernel': ['rbf','linear']},
     cv=5, return_train_score=False, n_iter=2)
 rs.fit(iris.data, iris.target)
 df2 = pd.DataFrame(rs.cv_results_)
 df2.to_csv('results_random.csv', index=False)
 
-
-from sklearn import svm
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
 
 model_params = {
     'svm': {
@@ -43,21 +39,17 @@ model_params = {
         'params' : {
             'C': [1,10,20],
             'kernel': ['rbf','linear']
-        }
-    },
+        }},
     'random_forest': {
         'model': RandomForestClassifier(),
         'params' : {
             'n_estimators': [1,5,10]
-        }
-    },
+        }},
     'logistic_regression' : {
         'model': LogisticRegression(solver='liblinear',multi_class='auto'),
         'params': {
             'C': [1,5,10]
-        }
-    }
-}
+        }}}
 
 scores = []
 
